2f1798b0f6ba6a1a44571527fb7959498722938c
horizonteMedic <sistemas@horizontemedic.com>
2024-04-25 22:07:43 -0500

    agregando anotacion test

--- src/test/java/hmDeveloper/developerHm/DeveloperHmApplicationTests.java
+++ src/test/java/hmDeveloper/developerHm/DeveloperHmApplicationTests.java
@@ -3,7 +3,7 @@ package hmDeveloper.developerHm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DeveloperHmApplicationTests.class)
 class DeveloperHmApplicationTests {
 
 	@Test
