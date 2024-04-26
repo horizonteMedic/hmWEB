ee7a2ef172e00d2ed05b384add7f8d64e1bd68d7
horizonteMedic <sistemas@horizontemedic.com>
2024-04-25 20:16:23 -0500

    archivos de configuracion

---
+++ src/main/java/hmDeveloper/developerHm/models/azure/config/DataSourceConfiguration.java
@@ -0,0 +1,18 @@
+package hmDeveloper.developerHm.models.azure.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public HikariDataSource customDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
